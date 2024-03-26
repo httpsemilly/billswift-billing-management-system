d473c934e27d2c26d38435a4e34b971218fd187c
Emilly Cavalcante <emilly.menezescs@gmail.com>
2024-03-25 23:58:37 -0300

    set up project structure and configurations

---
+++ src/test/java/com/uj/billswift/BillswiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uj.billswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillswiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
