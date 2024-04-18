9c789615cf7aa7699f8721a99d86ec9aaee73bc0
Emilly Cavalcante <emilly.menezescs@gmail.com>
2024-04-18 19:00:59 -0300

    create company repository interface

---
+++ src/main/java/com/uj/billswift/company/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.uj.billswift.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import com.uj.billswift.company.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, String>{
+    
+}
