e7be58486920e9f326aa25188d99db225a2e29a1
Emilly Cavalcante <emilly.menezescs@gmail.com>
2024-04-18 18:42:19 -0300

    add entity annotation and id field with uuid generation strategy

--- src/main/java/com/uj/billswift/company/Company.java
+++ src/main/java/com/uj/billswift/company/Company.java
@@ -1,5 +1,22 @@
 package com.uj.billswift.company;
 
+import jakarta.persistence.*;
+
+@Entity
+// @Table(name = "companies")
+
 public class Company {
-    
+    // definir informações sobre a empresa, como nome, cnpj, email, senha etc
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    // definir getters e setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
