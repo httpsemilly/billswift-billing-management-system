d4964a35c5f44bdccbf5f15ec79058884f0178ed
Emilly Cavalcante <emilly.menezescs@gmail.com>
2024-07-03 18:51:31 -0300

    update repository

--- src/main/java/com/uj/billswift/clients/ClientsRepository.java
+++ src/main/java/com/uj/billswift/clients/ClientsRepository.java
@@ -1,18 +1,12 @@
-/* package com.uj.billswift.clients;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientsRepository extends JpaRepository<Clients, Long> {
-} */
-
 package com.uj.billswift.clients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.*;
 
 public interface ClientsRepository extends JpaRepository<Clients, Long> {
     Optional<List<Clients>> findByCompanyId(String companyId);
     Optional<Clients> findByIdAndCompanyId(Long id, String companyId);
+    Clients findByName(String name);
+    
 }
