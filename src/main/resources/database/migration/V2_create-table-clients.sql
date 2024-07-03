3d79316129b8225f909c75fe074c2edf1173f509
Emilly Cavalcante <emilly.menezescs@gmail.com>
2024-07-03 18:59:24 -0300

    add new columns

--- src/main/resources/database/migration/V2_create-table-clients.sql
+++ src/main/resources/database/migration/V2_create-table-clients.sql
@@ -1,9 +1,13 @@
 CREATE TABLE clients (
-    id VARCHAR(50) PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    cnpj VARCHAR(18) NOT NULL UNIQUE,
+    phone VARCHAR(30) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    state VARCHAR(50) NOT NULL,
+    neighborhood VARCHAR(255) NOT NULL,
+    zip_code VARCHAR(50) NOT NULL,
+    city VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
-    idCompany VARCHAR,
+    idCompany VARCHAR(50),
     FOREIGN KEY (idCompany) REFERENCES companies(id)
 );
